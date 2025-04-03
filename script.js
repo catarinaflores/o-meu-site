35fb1ac865caaa56344120756f90a65f231ccea1
catarinaflores <catarina10flores@gmail.com>
2025-04-03 17:40:19 +0100

    added image grid

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+let me = "Catarina";
+
+let age = 25;
+
+let ageMultiplied = age * 7;
+
+console.log(ageMultiplied.toString(), "is my age multiplied by 7");
+
+let friends = ["Miriam", "Leonor", "David"];
+
+let object = {
+    name: "Catarina",
+    age: 25,
+    isStudent: true,
+};
+
+let friendChar = [
+    {
+        name: "Miriam",
+        age: 23,
+        isStudent: false,
+    },
+    {
+        name: "Leonor",
+        age: 19,
+        isStudent: true,
+    },
+    {
+        name: "David",
+        age: 23,
+        isStudent: true,
+    },
+];
