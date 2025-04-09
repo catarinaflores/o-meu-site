7bd373d91664ab768f2534bfff1592948964d231
catarinaflores <catarina10flores@gmail.com>
2025-04-09 23:52:51 +0100

    mockup
    light experiments

--- light/button.js
+++ light/button.js
@@ -1,4 +1,4 @@
-// This function is called when the button is clicked
+
 function toggleImage() {
   const img = document.getElementById("light");
   const button = document.getElementById("switch");
@@ -8,9 +8,11 @@ function toggleImage() {
     img.src = "https://i.postimg.cc/6QyTynzr/bulb-on.png";
     button.innerHTML = "Turn me off";
     body.classList.add("light-on");
+    img.classList.add("glow");
   } else {
     img.src = "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
     button.innerHTML = "Turn me on";
     body.classList.remove("light-on");
+    img.classList.remove("glow");
   }
 }
